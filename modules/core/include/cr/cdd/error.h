35b7a1c673eb91ce45e8499a37fbb8368882a8e0
dayn9t <dayn9t@gmail.com>
2024-07-31 19:22:41 +0800

    24-07-31

--- modules/core/include/cr/cdd/error.h
+++ modules/core/include/cr/cdd/error.h
@@ -2,17 +2,17 @@
 
 #include "basic.h"
 
-/// 没有错误
-#define CXR_OK					        0x0000
-/// 未知错误
-#define CXR_UNKNOWN_ERR					0x0001
-/// 违反契约错误
-#define CXR_DBC_ERR					    0x0002
+/// CR错误代码 - 正常/无错
+#define CRC_OK					        0x0000
+/// CR错误代码 - 未知
+#define CRC_UNKNOWN_ERR					0x0001
+/// CR错误代码 - 违反契约
+#define CRC_DBC_ERR					    0x0002
 
-/// STL异常
-#define CXR_STD_EXCEPTION				0x0011
-/// 未知异常
-#define CXR_UNKNOWN_EXCEPTION			0x0012
+/// CR错误代码 - STL异常
+#define CRC_STD_EXCEPTION				0x0011
+/// CR错误代码 - 未知异常
+#define CRC_UNKNOWN_EXCEPTION			0x0012
 
 /// C接口返回的错误信息
 typedef struct CrErrorImp {
@@ -27,3 +27,4 @@ typedef struct CrErrorImp {
     /// 错误相关文本信息, 必须是静态生命周期
     const char *message;
 } CrError;
+
--- modules/core/include/cr/dbc.hpp
+++ modules/core/include/cr/dbc.hpp
@@ -10,19 +10,19 @@
 #define cr_error(code) cr::error((code), __LINE__, __FILE__)
 
 /// 创建CrError的正确状态
-#define cr_ok() cr_error(CXR_OK)
+#define cr_ok() cr_error(CRC_OK)
 
 /// 创建CrError - 未知错误
-#define cr_unknown_error() cr_error(CXR_UNKNOWN_ERR)
+#define cr_unknown_error() cr_error(CRC_UNKNOWN_ERR)
 
 /// 创建CrError - 未知异常
-#define cr_unknown_exception() cr_error(CXR_UNKNOWN_EXCEPTION)
+#define cr_unknown_exception() cr_error(CRC_UNKNOWN_EXCEPTION)
 
 /// 创建出错信息 - 附带描述信息
 #define cr_error_msg(code, msg) cr::error((code), __LINE__, __FILE__, 0, 0, (msg))
 
 /// 创建出错信息 - 从std::exception, TODO: e.what() 可能不是静态生存周期
-#define cr_std_exception(e) cr::error(CXR_STD_EXCEPTION, __LINE__, __FILE__, 0, 0, (e).what())
+#define cr_std_exception(e) cr::error(CRC_STD_EXCEPTION, __LINE__, __FILE__, 0, 0, (e).what())
 
 /// 创建出错信息 - 附带数据
 #define cr_error_data(code, data1, data2) cr::error((code), __LINE__, __FILE__, (data1), (data2))
@@ -34,7 +34,7 @@
 #pragma region 错误抛出
 
 /// 确保条件成功，否则抛出 CrError(CX_UNKNOWN_ERR)
-#define cr_ensure(cond) while(!(cond)) { throw cr_error_msg(CXR_DBC_ERR, #cond); }
+#define cr_ensure(cond) while(!(cond)) { throw cr_error_msg(CRC_DBC_ERR, #cond); }
 
 /// 确保条件成功，否则抛出 CrError(code)
 #define cr_ensure_or(cond, code) while(!(cond)) { throw cr_error_msg(code, #cond); }
@@ -51,7 +51,7 @@
 /// 确保条件成功，否则抛出 CrError(code, data1, data2, msg)
 #define cr_ensure_or_all(cond, code, data1, data2, msg) while(!(cond)) { throw cr_error_all(code, data1, data2, msg); }
 
-#ifdef CXR_DBC_NO_PREFIX
+#ifdef CRC_DBC_NO_PREFIX
 #define ensure(cond) cr_ensure(cond)
 #define ensure_or(cond, code) cr_ensure_or(cond, code)
 #define ensure_or_msg(cond, code, msg) cr_ensure_or_msg(cond, code, msg)
@@ -81,10 +81,15 @@
 #pragma endregion
 
 
-#ifndef CXR_FILEPATH_CUT_N
+#ifndef CRC_FILEPATH_CUT_N
 /// 文件路径截调前N段, 避免路径过长, 减少信息泄漏
-#define CXR_FILEPATH_CUT_N 3
+#define CRC_FILEPATH_CUT_N 3
 #endif
+
+
+/// 无错误
+#define CRE_OK  CrError{}
+
 
 namespace cr {
 
@@ -93,13 +98,13 @@ namespace cr {
 
     /// 生成错误信息
     inline CrError error(uint32_t code, uint32_t line, const char *file, int data1=0, int data2=0, const char *message= nullptr) {
-        CrError e = {code, line, {data1, data2}, cut_path(file, CXR_FILEPATH_CUT_N), message};
+        CrError e = {code, line, {data1, data2}, cut_path(file, CRC_FILEPATH_CUT_N), message};
         return e;
     }
 
     /// 判定CrError包含正常状态
     inline bool is_ok(const CrError& err) {
-        return err.code == CXR_OK;
+        return err.code == CRC_OK;
     }
 
     /// 生成错误信息的字符串表示
