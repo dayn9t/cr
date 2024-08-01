1a94ac6da80734e3a02b7ef4aea600e336dc36b1
dayn9t <dayn9t@gmail.com>
2024-08-01 16:01:02 +0800

    24-08-01

--- modules/core/src/cdd_adt.cpp
+++ modules/core/src/cdd_adt.cpp
@@ -1,13 +1,32 @@
 #include <cr/cdd_adt.hpp>
 #include <boost/url.hpp>
+#include <iostream>
 
 using Url = boost::url;
 
+using namespace std;
+
 namespace cr
 {
+    void show_parts(const Url& url)
+    {
+        cout << "    Scheme: " << url.scheme() << endl;
+        cout << "    User: " << url.user() << endl;
+        cout << "    Password: " << url.password() << endl;
+        cout << "    Host: " << url.host() << endl;
+        cout << "    Port: " << url.port() << endl;
+        cout << "    Path: " << url.path() << endl;
+        cout << "    Query: " << url.query() << endl;
+        //cout << "Params: " <<  url.params() << endl;
+        //cout << "Fragment: " <<  url.fragment() << endl;
+    }
+
     bool parse_url(InString& url_str, CrEndpoint& endpoint, CrAuthInfo& auth, StrMap& params)
     {
         auto url = Url(url_str);
+
+        show_parts(url);
+
         strncpy(endpoint.hostname, url.host().data(), CR_HOSTNAME_MAX_LEN);
         endpoint.port_number = url.port_number();
 
